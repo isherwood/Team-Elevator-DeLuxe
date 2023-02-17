d18f9f2e63ae104c544c01f069b5766f390c3ba9
Clint Buhs <clint.buhs@stearnsbank.com>
2023-02-17 13:30:24 -0600

    Elevator shaft, styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Button, Col, Container, Offcanvas, Row} from 'react-bootstrap';
+import {Button, Col, Container, Form, Offcanvas, Row} from 'react-bootstrap';
 import {TfiHelpAlt} from "react-icons/tfi";
 
 import './App.css';
@@ -7,6 +7,8 @@ import Elevator from "./components/Elevator/Elevator";
 
 function App() {
     const [showOffCanvas, setShowOffCanvas] = useState(false);
+    const [showElevator, setShowElevator] = useState(true);
+
     const handleHideOffCanvas = () => setShowOffCanvas(false);
     const handleShowOffCanvas = () => setShowOffCanvas(true);
 
@@ -14,9 +16,10 @@ function App() {
         <Container fluid className='app-container d-flex flex-column vh-100'>
             <Row>
                 <Col className='position-relative'>
-                    <h1 className='display-5 fst-italic text-center'>Team Elevator De Luxe</h1>
+                    <h1 className='display-5 fst-italic text-center mt-2'>Team Elevator De Luxe</h1>
 
-                    <Button variant='link' size='lg' className='position-absolute top-0 end-0' onClick={handleShowOffCanvas}>
+                    <Button variant='link' size='lg' className='position-absolute top-0 end-0 mt-sm-2 mt-md-3'
+                            onClick={handleShowOffCanvas}>
                         <TfiHelpAlt/>
                     </Button>
                 </Col>
@@ -27,13 +30,25 @@ function App() {
                     </Offcanvas.Header>
 
                     <Offcanvas.Body>
-                        <p>Click an elevator level to increase its count. Click a number to reduce its count.</p>
+                        <p>Click an elevator level to increase its count. Click a count to reduce it.</p>
+
+                        <Form.Group className='mb-3'>
+                            <Form.Check type="checkbox" id='randomizePlayersCheckbox' label="Show team elevation"
+                                        defaultChecked={showElevator}
+                                        onClick={e => setShowElevator(e.currentTarget.checked)}></Form.Check>
+                        </Form.Group>
                     </Offcanvas.Body>
                 </Offcanvas>
             </Row>
 
             <Row className='flex-grow-1 justify-content-center align-items-center'>
-                <Elevator/>
+                <Elevator showElevator={showElevator}/>
+            </Row>
+
+            <Row>
+                <Col className='pb-3'>
+                    <div className='text-nowrap text-muted text-center'>A SeaBee Software Production</div>
+                </Col>
             </Row>
         </Container>
     );
