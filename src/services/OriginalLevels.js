5c6b165229512d34e802b5a2b53c1f2be1c71f61
Clint Buhs <clint@planetbuhs.net>
2025-02-14 15:46:25 -0600

    Fixing level IDs

--- src/services/OriginalLevels.js
+++ src/services/OriginalLevels.js
@@ -5,16 +5,16 @@ const originalLevels = [
     {id: 4, label: 'innovative'},
     {id: 5, label: 'productive'},
     {id: 6, label: 'hopeful'},
-    {id: 8, label: 'flexible'},
-    {id: 9, label: 'curious'},
-    {id: 10, label: 'frustrated'},
+    {id: 7, label: 'flexible'},
+    {id: 8, label: 'curious'},
+    {id: 9, label: 'frustrated'},
     {id: 10, label: 'impatient'},
-    {id: 12, label: 'anxious'},
-    {id: 13, label: 'uncertain'},
-    {id: 11, label: 'overwhelmed'},
-    {id: 15, label: 'exhausted'},
-    {id: 16, label: 'irritable'},
-    {id: 17, label: 'gloomy'}
+    {id: 11, label: 'anxious'},
+    {id: 12, label: 'uncertain'},
+    {id: 13, label: 'overwhelmed'},
+    {id: 14, label: 'exhausted'},
+    {id: 15, label: 'irritable'},
+    {id: 16, label: 'gloomy'}
 ];
 
 export default originalLevels;
